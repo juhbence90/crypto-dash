8112cb0c7f0e7f16a1f1dc6b05b2e219bfc2b761
Juhasz Bence <juhbence90@gmail.com>
2025-12-17 18:50:09 +0100

    Initial commit

---
+++ src/pages/about.jsx
@@ -0,0 +1,13 @@
+const AboutPage = () => {
+  return (
+    <div className="about">
+      <h1>About Crypto Dash</h1>
+      <p>
+        Crypto Dash is a simple React application that displays live
+        cryptocurrency data using the CoinGecko API.
+      </p>
+    </div>
+  );
+};
+
+export default AboutPage;
